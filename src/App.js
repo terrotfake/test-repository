85f7d8167af364907301fecf9536907d24578466
terrotfake <terotfake01@gmail.com>
2024-11-18 14:14:56 +0300

    Исправил ошибку с линтером и тестами

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Это тестовый проект</p>
          {name}
       </header>
     </div>
