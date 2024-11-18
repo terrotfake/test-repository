4ea7cf6514d7f1a45b0ae90f183fda493ecf70bf
terrotfake <terotfake01@gmail.com>
2024-11-18 14:01:39 +0300

    Добавил своё ФИО в переменную

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const name = "Бецко Д.С.";
 
   console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         {greeting}
+         {name}
       </header>
     </div>
   );
