038f1315b0ef44b27a7d25c05ff50283564faac7
terrotfake <terotfake01@gmail.com>
2024-11-21 10:43:05 +0300

    Fix unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders greeting message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Это тестовый проект/i);
+  const linkElement = screen.getByText(/Добропожаловать в мой проект/i);
   expect(linkElement).toBeInTheDocument();
 });
